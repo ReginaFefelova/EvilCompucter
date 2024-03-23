c3b320d4e4aacde976fb23f1bcefd1afdff73072
Anton <a.bawin@yandex.ru>
2024-03-24 02:35:14 +0300

    Refactoring | ANSI_COLOR -> SmoothText.java

--- src/SmoothText.java
+++ src/SmoothText.java
@@ -1,8 +1,10 @@
-import java.util.concurrent.TimeUnit;
-
 public class SmoothText {
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_BLACK = "\u001B[30m";
+    private static final String ANSI_GREEN = "\u001B[32m";
     public static void print(String text)
     {
+        System.out.print(ANSI_GREEN);
         for(Character chr : text.toCharArray())
         {
             System.out.print(chr);
