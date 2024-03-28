819f06a6c43a08282cd8ff0a3eaa56e55c646ec3
Anton <a.bawin@yandex.ru>
2024-03-29 01:57:20 +0300

    Supress error

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#Скрипт сборки и запуска под Debian-like
+#Сделать исполняемым chmod +x
+#При запуске аргументом указать путь до папки src
+#В случае установки jdk-17 при вызове update-alternatives и наличии иных версий выбрать 17 по-умолчанию
 set +x
 update-alternatives --list java 2> /dev/null  | grep -o java-17 1> /dev/null
 JAVA_INSTALLED=$(echo $?)
