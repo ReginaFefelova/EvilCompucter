da628e78572ed77e4fe47e19f001fd05f4ee7671
Anton <a.bawin@yandex.ru>
2024-03-29 01:49:26 +0300

    Supress error

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#Сборка и запуск под Debian-like
 set +x
-update-alternatives --list java | grep -o java-17 1> /dev/null
+update-alternatives --list java 2> /dev/null  | grep -o java-17 1> /dev/null
 JAVA_INSTALLED=$(echo $?)
 if ! [[ "$JAVA_INSTALLED" == 0 ]]; then
-	sudo apt update;sudo apt install openjdk-17-jdk
+	sudo apt update;sudo apt install -y  openjdk-17-jdk
 fi
 cd $1
 javac -encoding utf-8 *.java
